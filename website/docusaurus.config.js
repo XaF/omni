2c9e5d606cce9b33dda50efc5c9ed884d48c51ef
Raphaël Beamonte <raphael.beamonte@gmail.com>
2023-07-09 22:33:11 -0400

    📝 website: Fix baseUrl

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: 'https://omnicli.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '',
+  baseUrl: '/',
 
   trailingSlash: false,
 
