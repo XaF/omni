XaF/omni

commit 7a99db09b2f4358da443230b002b5b5f98d41555

✨ Use nodejs-extended semver for version parsing
Closes https://github.com/XaF/omni/issues/119

src/internal/config/up/asdf_base.rs
@@ -4,8 +4,11 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
+use std::str::FromStr;
 
 use lazy_static::lazy_static;
+use node_semver::Range as semverRange;
+use node_semver::Version as semverVersion;
 use once_cell::sync::OnceCell;
 use serde::Deserialize;
 use serde::Serialize;
@@ -876,14 +879,25 @@ impl UpConfigAsdfBase {
 }
 
 fn version_match(expect: &str, version: &str) -> bool {
-    let rest_of_line = if expect == "latest" {
-        version
-    } else if version.starts_with(&expect) {
-        version.strip_prefix(&expect).unwrap()
-    } else {
-        return false;
-    };
+    if expect == "latest" {
+        return true;
+    }
 
+    if let Ok(requirements) = semverRange::from_str(expect) {
+        if let Ok(version) = semverVersion::from_str(version) {
+            // By not directly returning, we allow to keep the prefix
+            // check in case the version is not a semver version
+            if version.satisfies(&requirements) {
+                return true;
+            }
+        }
+    }
+
+    if !version.starts_with(&expect) {
+        return false;
+    }
+
+    let rest_of_line = version.strip_prefix(&expect).unwrap();
     rest_of_line.chars().all(|c| c.is_digit(10) || c == '.')
 }
 

Cargo.toml
@@ -36,6 +36,7 @@ itertools = "0.11.0"
 lazy_static = "1.4.0"
 libz-sys = { version = "1.1.12", features = ["static"] }
 machine-uid = "0.5.1"
+node-semver = "2.1.0"
 once_cell = "1.18.0"
 openssl = { version = "0.10", features = ["vendored"] }
 package-json = "0.4.0"

src/internal/config/up/nodejs.rs
@@ -1,7 +1,8 @@
 use std::path::PathBuf;
+use std::str::FromStr;
 
+use node_semver::Range as semverRange;
 use package_json::PackageJsonManager;
-use semver::VersionReq;
 use serde::{Deserialize, Serialize};
 
 use crate::internal::config::up::UpConfigAsdfBase;
@@ -62,74 +63,8 @@ fn detect_version_from_package_json(_tool_name: String, path: PathBuf) -> Option
     let engines = pkgfile.engines.clone().unwrap();
 
     if let Some(node_version) = engines.get("node") {
-        if let Ok(requirements) = VersionReq::parse(node_version) {
-            for comparator in requirements.comparators {
-                match comparator.op {
-                    semver::Op::Exact
-                    | semver::Op::Tilde
-                    | semver::Op::Wildcard
-                    | semver::Op::LessEq => {
-                        let mut version = if let (Some(minor), Some(patch)) =
-                            (comparator.minor, comparator.patch)
-                        {
-                            format!("{}.{}.{}", comparator.major, minor, patch)
-                        } else if let Some(minor) = comparator.minor {
-                            format!("{}.{}", comparator.major, minor)
-                        } else {
-                            format!("{}", comparator.major)
-                        };
-
-                        if comparator.pre != semver::Prerelease::EMPTY {
-                            version = format!("{}-{}", version, comparator.pre.as_str());
-                        }
-
-                        return Some(version);
-                    }
-                    semver::Op::Caret => {
-                        let major = comparator.major;
-                        let mut minor = comparator.minor.unwrap_or(0);
-                        let mut patch = comparator.patch.unwrap_or(0);
-
-                        if major > 0 {
-                            minor = 0;
-                            patch = 0;
-                        } else if minor > 0 {
-                            patch = 0;
-                        }
-
-                        let parts = vec![major, minor, patch];
-                        let version = parts
-                            .iter()
-                            .filter(|part| **part > 0)
-                            .map(|part| part.to_string())
-                            .collect::<Vec<String>>()
-                            .join(".");
-
-                        return Some(version);
-                    }
-                    semver::Op::Less => {
-                        let version = if let (Some(minor), Some(patch)) =
-                            (comparator.minor, comparator.patch)
-                        {
-                            format!("{}.{}.{}", comparator.major, minor, patch - 1)
-                        } else if let Some(minor) = comparator.minor {
-                            format!("{}.{}", comparator.major, minor - 1)
-                        } else {
-                            format!("{}", comparator.major - 1)
-                        };
-
-                        return Some(version);
-                    }
-                    semver::Op::Greater | semver::Op::GreaterEq => {
-                        // Nothing to do, we can still install the latest
-                    }
-                    _ => {
-                        unreachable!();
-                    }
-                }
-
-                return Some("latest".to_string());
-            }
+        if let Ok(_requirements) = semverRange::from_str(node_version) {
+            return Some(node_version.to_string());
         }
     }
 

commit 0e272dfd5d38079554d35e09306bb6a6787064d3

🐛 Check the version when using 'latest'
Fixes regression introduced in 7a99db09b2f4358da443230b002b5b5f98d41555
Fixes https://github.com/XaF/omni/issues/127

src/internal/config/up/asdf_base.rs
@@ -880,6 +880,19 @@ impl UpConfigAsdfBase {
 
 fn version_match(expect: &str, version: &str) -> bool {
     if expect == "latest" {
+        let mut prev = '.';
+        for c in version.chars() {
+            if !c.is_digit(10) {
+                if c == '.' {
+                    if prev == '.' {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+            prev = c;
+        }
         return true;
     }
 