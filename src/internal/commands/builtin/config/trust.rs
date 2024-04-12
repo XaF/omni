b8af61923bc02408788aeb1bbbcafdfb53ab6a20
Raphaël Beamonte <raphael.beamonte@gmail.com>
2024-04-12 10:43:11 -0400

    clippy fixes

--- src/internal/commands/builtin/config/trust.rs
+++ src/internal/commands/builtin/config/trust.rs
@@ -174,7 +174,7 @@ impl BuiltinCommand for ConfigTrustCommand {
         }
 
         let path = if let Some(repo) = &self.cli_args().repository {
-            if let Some(repo_path) = ORG_LOADER.find_repo(&repo, true, false) {
+            if let Some(repo_path) = ORG_LOADER.find_repo(repo, true, false) {
                 repo_path
             } else {
                 omni_error!(format!("repository not found: {}", repo));
