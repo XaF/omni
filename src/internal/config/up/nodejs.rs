7d4875c35cadb27a5d6a459a3e3c8c098768fcaf
Raphaël Beamonte <raphael.beamonte@gmail.com>
2024-07-02 00:40:37 -0400

    Add install_engines and install_packages configuration parameters for nodejs

--- src/internal/config/up/nodejs.rs
+++ src/internal/config/up/nodejs.rs
@@ -8,6 +8,7 @@ use serde::Deserialize;
 use serde::Serialize;
 use tokio::process::Command as TokioCommand;
 
+use crate::internal::cache::utils as cache_utils;
 use crate::internal::cache::utils::CacheObject;
 use crate::internal::cache::UpEnvironmentsCache;
 use crate::internal::config::up::utils::data_path_dir_hash;
@@ -24,9 +25,56 @@ use crate::internal::env::current_dir;
 use crate::internal::workdir;
 use crate::internal::ConfigValue;
 
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct UpConfigNodejsParams {
+    #[serde(
+        default = "cache_utils::set_true",
+        skip_serializing_if = "cache_utils::is_true"
+    )]
+    install_engines: bool,
+    #[serde(
+        default = "cache_utils::set_true",
+        skip_serializing_if = "cache_utils::is_true"
+    )]
+    install_packages: bool,
+}
+
+impl Default for UpConfigNodejsParams {
+    fn default() -> Self {
+        Self {
+            install_engines: Self::DEFAULT_INSTALL_ENGINES,
+            install_packages: Self::DEFAULT_INSTALL_PACKAGES,
+        }
+    }
+}
+
+impl UpConfigNodejsParams {
+    const DEFAULT_INSTALL_ENGINES: bool = true;
+    const DEFAULT_INSTALL_PACKAGES: bool = true;
+
+    pub fn from_config_value(config_value: Option<&ConfigValue>) -> Self {
+        let mut params = Self::default();
+
+        if let Some(config_value) = &config_value {
+            if let Some(value) = config_value.get_as_bool("install_engines") {
+                params.install_engines = value;
+            }
+
+            if let Some(value) = config_value.get_as_bool("install_packages") {
+                params.install_packages = value;
+            }
+        }
+
+        params
+    }
+}
+
 #[derive(Debug, Deserialize, Clone)]
 pub struct UpConfigNodejs {
+    #[serde(skip)]
     pub asdf_base: UpConfigAsdfBase,
+    #[serde(skip)]
+    pub params: UpConfigNodejsParams,
 }
 
 impl Serialize for UpConfigNodejs {
@@ -34,7 +82,20 @@ impl Serialize for UpConfigNodejs {
     where
         S: ::serde::ser::Serializer,
     {
-        self.asdf_base.serialize(serializer)
+        // Serialize object into serde_json::Value
+        let mut nodejs_base = serde_json::to_value(&self.asdf_base).unwrap();
+
+        // Serialize the params object
+        let nodejs_params = serde_json::to_value(&self.params).unwrap();
+
+        // Merge the params object into the base object
+        nodejs_base
+            .as_object_mut()
+            .unwrap()
+            .extend(nodejs_params.as_object().unwrap().clone());
+
+        // Serialize the object
+        nodejs_base.serialize(serializer)
     }
 }
 
@@ -45,7 +106,9 @@ impl UpConfigNodejs {
         asdf_base.add_detect_version_func(detect_version_from_nvmrc);
         asdf_base.add_post_install_func(setup_individual_npm_prefix);
 
-        Self { asdf_base }
+        let params = UpConfigNodejsParams::from_config_value(config_value);
+
+        Self { asdf_base, params }
     }
 
     pub fn up(
@@ -118,7 +181,7 @@ fn detect_version_from_nvmrc(_tool_name: String, path: PathBuf) -> Option<String
 
 fn setup_individual_npm_prefix(
     progress_handler: &dyn ProgressHandler,
-    _config_value: Option<ConfigValue>,
+    config_value: Option<ConfigValue>,
     tool: String,
     tool_real_name: String,
     _requested_version: String,
@@ -200,7 +263,13 @@ fn setup_individual_npm_prefix(
         }
     };
 
-    // Handle auto-installing the right engines in the right versions
+    let params = UpConfigNodejsParams::from_config_value(config_value.as_ref());
+    if !params.install_engines && !params.install_packages {
+        // Exit early if we don't need to install engines or packages
+        return Ok(());
+    }
+
+    // Handle auto-installing the right engines in the right versions, and the packages
     for version in &versions {
         for dir in &version.dirs {
             let actual_dir = PathBuf::from(workdir_root).join(dir);
@@ -236,70 +305,74 @@ fn setup_individual_npm_prefix(
             // Load the environment for that directory
             update_dynamic_env_for_command(actual_dir.to_str().unwrap());
 
-            // Install the engines
-            for (engine, version_range) in pkgfile.engines.iter() {
-                if engine == "node" || engine == "iojs" {
+            if params.install_engines {
+                // Install the engines
+                for (engine, version_range) in pkgfile.engines.iter() {
+                    if engine == "node" || engine == "iojs" {
+                        continue;
+                    }
+
+                    progress_handler
+                        .progress(format!("installing {} version {}", engine, version_range));
+
+                    // Install the engine using directly the provided version range
+                    let mut npm_install = TokioCommand::new("npm");
+                    npm_install.arg("install");
+                    npm_install.arg("-g");
+                    npm_install.arg(&format!("{}@{}", engine, version_range));
+                    npm_install.stdout(std::process::Stdio::piped());
+                    npm_install.stderr(std::process::Stdio::piped());
+
+                    let result = run_progress(
+                        &mut npm_install,
+                        Some(progress_handler),
+                        RunConfig::default(),
+                    );
+
+                    if let Err(e) = result {
+                        let msg = format!(
+                            "failed to install engine {} version {}: {}",
+                            engine, version_range, e
+                        );
+                        progress_handler.error_with_message(msg.clone());
+                        return Err(UpError::Exec(msg));
+                    }
+                }
+            }
+
+            if params.install_packages {
+                // Install the packages
+                let engines_slice: Vec<String> = pkgfile.engines.keys().cloned().collect();
+                let install_engines = PackageInstallEngine::all_sorted(&actual_dir, &engines_slice);
+                let install_engine = install_engines.first().unwrap();
+
+                if which::which(install_engine.name()).is_err() {
+                    progress_handler.progress(format!(
+                        "skipping package installation: {} not found",
+                        install_engine.name(),
+                    ));
                     continue;
                 }
 
-                progress_handler
-                    .progress(format!("installing {} version {}", engine, version_range));
+                progress_handler.progress(format!(
+                    "installing packages with {}",
+                    install_engine.name(),
+                ));
 
-                // Install the engine using directly the provided version range
-                let mut npm_install = TokioCommand::new("npm");
-                npm_install.arg("install");
-                npm_install.arg("-g");
-                npm_install.arg(&format!("{}@{}", engine, version_range));
-                npm_install.stdout(std::process::Stdio::piped());
-                npm_install.stderr(std::process::Stdio::piped());
+                let mut pkg_install = install_engine.install_command();
+                pkg_install.current_dir(&actual_dir);
 
                 let result = run_progress(
-                    &mut npm_install,
+                    &mut pkg_install,
                     Some(progress_handler),
                     RunConfig::default(),
                 );
 
                 if let Err(e) = result {
-                    let msg = format!(
-                        "failed to install engine {} version {}: {}",
-                        engine, version_range, e
-                    );
+                    let msg = format!("failed to install packages: {}", e);
                     progress_handler.error_with_message(msg.clone());
                     return Err(UpError::Exec(msg));
                 }
-            }
-
-            // Install the packages
-            let engines_slice: Vec<String> = pkgfile.engines.keys().cloned().collect();
-            let install_engines = PackageInstallEngine::all_sorted(&actual_dir, &engines_slice);
-            let install_engine = install_engines.first().unwrap();
-
-            if which::which(install_engine.name()).is_err() {
-                progress_handler.progress(format!(
-                    "skipping package installation: {} not found",
-                    install_engine.name(),
-                ));
-                continue;
-            }
-
-            progress_handler.progress(format!(
-                "installing packages with {}",
-                install_engine.name(),
-            ));
-
-            let mut pkg_install = install_engine.install_command();
-            pkg_install.current_dir(&actual_dir);
-
-            let result = run_progress(
-                &mut pkg_install,
-                Some(progress_handler),
-                RunConfig::default(),
-            );
-
-            if let Err(e) = result {
-                let msg = format!("failed to install packages: {}", e);
-                progress_handler.error_with_message(msg.clone());
-                return Err(UpError::Exec(msg));
             }
         }
     }
