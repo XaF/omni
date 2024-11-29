f01ec3efe1e610cbd4b91b0d219282a32ab943df
Raphaël Beamonte <raphael.beamonte@gmail.com>
2024-11-29 16:20:29 -0500

    feat(`help`): ✨ add `--output` flag which can take `plain` or `json` (#810)
    This allows to get the help messages in a programmatically parseable
    way, which can open the door for generating documentation from the help
    output of omni.
    
    Closes https://github.com/XaF/omni/issues/809

--- src/internal/cache/utils.rs
+++ src/internal/cache/utils.rs
@@ -27,3 +27,7 @@ pub fn origin_of_time() -> OffsetDateTime {
 pub fn is_origin_of_time(value: &OffsetDateTime) -> bool {
     *value == origin_of_time()
 }
+
+pub fn is_zero(x: &usize) -> bool {
+    *x == 0
+}
