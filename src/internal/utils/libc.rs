3854573971d83c9870a685275c0a6131ec419305
Raphaël Beamonte <raphael.beamonte@gmail.com>
2025-01-27 17:14:08 -0500

    chore(`musl`): add more files to check
    To support ARM environments, even if the fallback to `ldd` should work.

--- src/internal/utils/libc.rs
+++ src/internal/utils/libc.rs
@@ -9,11 +9,14 @@ pub fn detect_libc() -> bool {
     // First try filesystem check as it's faster
     if Path::new("/lib64/ld-linux-x86-64.so.2").exists()
         || Path::new("/lib32/ld-linux.so.2").exists()
+        || Path::new("/lib/ld-linux-aarch64.so.1").exists()
     {
         return true;
     }
 
-    if Path::new("/lib/ld-musl-x86_64.so.1").exists() {
+    if Path::new("/lib/ld-musl-x86_64.so.1").exists()
+        || Path::new("/lib/ld-musl-aarch64.so.1").exists()
+    {
         return false;
     }
 
